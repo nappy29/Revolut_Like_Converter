e5bc477adfe6473bf40d02395a05ba78678fa304
Nappy29 <ewaneenombe@gmail.com>
2020-01-06 16:33:06 +0300

    code clean up

--- app/src/test/java/com/example/revolut/ui/main/MainViewModelTest.java
+++ app/src/test/java/com/example/revolut/ui/main/MainViewModelTest.java
@@ -137,23 +137,13 @@ public class MainViewModelTest {
         list.add(new Currency("BGN", 1.9589));
         list.add(new Currency("BRL", 4.7995));
 
-        List<Currency> newlist = new ArrayList<>();
+        mainViewModel.onCurrencyItemClicked(1);
 
-        list.add(new Currency("EUR", 1.0));
-        list.add(new Currency("AUD", 1.619));
-        list.add(new Currency("BGN", 1.9589));
-        list.add(new Currency("BRL", 4.7995));
-
-        List<Currency> listofCurrencies = mainViewModel.constructSourceList(list);
-
+        assertTrue(Objects.equals(mainViewModel.constructSourceList(list).get(0), new Currency("AUD", 1.619)));
 
         mainViewModel.onCurrencyItemClicked(1);
 
-        assertTrue(Objects.equals(listofCurrencies.get(0), new Currency("AUD", 1.619)));
-
-        mainViewModel.onCurrencyItemClicked(1);
-
-        assertTrue(Objects.equals(listofCurrencies.get(0), new Currency("EUR", 1)));
+        assertTrue(Objects.equals(mainViewModel.constructSourceList(list).get(0), new Currency("EUR", 1)));
 
         mainViewModel.onCurrencyItemClicked(0);
     }
